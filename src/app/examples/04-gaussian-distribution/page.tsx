15870cc6e0b64fff6918f5f1167b0fe6cde275e8
soxface <sonyacorcoran@gmail.com>
2025-04-19 13:48:04 +0930

    add: example 0.4 gaussian distribution

---
+++ src/app/examples/04-gaussian-distribution/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import P5Wrapper from '@/components/P5Wrapper';
+import SketchLayout from '@/components/SketchLayout';
+import p5 from 'p5';
+
+export default function GaussianDistribution() {
+    // Define the sketch function
+    const sketch = (p5: p5) => {
+
+        p5.setup = () => {
+            p5.createCanvas(640, 360);
+            p5.background(255);
+        };
+
+        p5.draw = () => {
+            const num = p5.randomGaussian(320, 60);
+
+            p5.noStroke();
+            p5.fill(0, 10);
+            p5.circle(num, 120, 16);
+
+        };
+    };
+
+    return (
+        <SketchLayout
+            title="Example 0.4: A Gaussian Distribution"
+            description="A normal distribution with mean 320 and Standard deviation of 60"
+        >
+            <P5Wrapper sketch={sketch} />
+        </SketchLayout>
+    );
+}
